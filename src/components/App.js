9ef7ac312acf002fdd6d63e4406989be611aa684
m1g <miguelmalcolm@outlook.com>
2016-12-09 00:11:34 -0500

    created a cards component

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,37 @@ class App extends Component {
 
   render () {
     return <div>
-      <h1>Hello, World!</h1>
+      <header>
+        <h1>Match Game</h1>
+      </header>
+      <table>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>6</td>
+            <td>7</td>
+            <td>8</td>
+          </tr>
+          <tr>
+            <td>9</td>
+            <td>10</td>
+            <td>11</td>
+            <td>12</td>
+          </tr>
+          <tr>
+            <td>13</td>
+            <td>14</td>
+            <td>15</td>
+            <td>16</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   }
 }
---
+++ src/components/Cards.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import characters from '../characters.json'
+
+class Cards extends Component {
+
+  render () {
+    const cards = this.state.cards.map(2 * (card, i) => {
+      const isFlipped = card.flip ? styles.flip : ''
+      return
+    })
+}
+
+export default Cards
